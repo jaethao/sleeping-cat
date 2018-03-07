0c91e3c3538a609586caadb710d57e59542e4b9d
jaethao <mjathao@gmail.com>
2018-03-07 15:15:18 -0600

    created files and server

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE cats
 	sleepy BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobbi');
+INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO cats (name) VALUES ('Owens');
